3c422e64bc66b25e28a88653ac489e47fd9dca85
Hugo <github@hugos.anonaddy.com>
2024-09-27 11:12:35 +0200

    prevent image preview on mobile devices

--- imagePreview.js
+++ imagePreview.js
@@ -1,26 +1,30 @@
 // Image on hover of projects
+const isMobile = /Mobi|Android/i.test(navigator.userAgent) || window.innerWidth < 768;
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("dom content loaded)");
-    const hoverLinks = document.querySelectorAll('.project-entry');
-    const previewImage = document.getElementById('preview-image');
+if (!isMobile) {
 
-    hoverLinks.forEach(div => {
-        div.addEventListener('mouseenter', () => {
-            const imageUrl = div.getAttribute('data-image');
-            previewImage.src = imageUrl; // Set the image source based on the hovered div
-            previewImage.style.display = 'block'; // Show the image when mouse enters the div
-        });
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log("dom content loaded)");
+        const hoverLinks = document.querySelectorAll('.project-entry');
+        const previewImage = document.getElementById('preview-image');
 
-        div.addEventListener('mouseleave', () => {
-            previewImage.style.display = 'none'; // Hide the image when mouse leaves the div
-        });
+        hoverLinks.forEach(div => {
+            div.addEventListener('mouseenter', () => {
+                const imageUrl = div.getAttribute('data-image');
+                previewImage.src = imageUrl; // Set the image source based on the hovered div
+                previewImage.style.display = 'block'; // Show the image when mouse enters the div
+            });
 
-        div.addEventListener('mousemove', (e) => {
-            // based on the size of the preview-image : 150x150px
-            previewImage.style.left = `${e.clientX - 75 }px`;
-            previewImage.style.top = `${e.clientY  - 75}px`;
+            div.addEventListener('mouseleave', () => {
+                previewImage.style.display = 'none'; // Hide the image when mouse leaves the div
+            });
 
+            div.addEventListener('mousemove', (e) => {
+                // based on the size of the preview-image : 150x150px
+                previewImage.style.left = `${e.clientX - 75}px`;
+                previewImage.style.top = `${e.clientY - 75}px`;
+
+            });
         });
     });
-});
+}
